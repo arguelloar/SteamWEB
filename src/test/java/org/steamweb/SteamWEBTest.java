e7b7c2f47cb1b19da0d11f45ebe8550e3abb52cc
AdrianArguelloR <arguello.ramosadrian@gmail.com>
2022-10-02 17:50:42 -0700

    First Version

---
+++ src/test/java/org/steamweb/SteamWEBTest.java
@@ -0,0 +1,36 @@
+package org.steamweb;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.steamweb.types.Item;
+
+class SteamWEBTest {
+
+    @Test
+    void setSession() {
+    }
+
+    @Test
+    void logout() {
+    }
+
+    @Test
+    void getFarmableGames() {
+    }
+
+    @Test
+    void getCardsInventory() {
+    }
+
+    @Test
+    void changeAvatar() {
+    }
+
+    @Test
+    void clearAliases() {
+    }
+
+    @Test
+    void changePrivacy() {
+    }
+}
